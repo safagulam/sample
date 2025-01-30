e00a2f5063269e83eada7040bcac68ae46c470bd
safagulam <safagulam105@gmail.com>
2025-01-30 14:25:11 +0530

    Add files via upload

---
+++ javamaven/src/main/java/com/sxcce/javamaven/Javamaven.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sxcce.javamaven;
+import java.util.*;
+/**
+ *
+ * @author IT20
+ */
+public class Javamaven {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int a =scan.nextInt();
+        int b =scan.nextInt();
+        
+        System.out.println("Adition of two numbers"+(a+b));
+    }
+}
